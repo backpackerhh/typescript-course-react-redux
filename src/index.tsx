92378e09bc0766b47b0360e78647ae0dd09af2ec
David Montesdeoca <backpackerhh@gmail.com>
2024-01-28 13:58:28 +0100

    Replace class component with functional component
    Add a couple of button to increment and decrement a counter as an
    example.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,30 @@
-import { Component, ReactNode } from "react";
-import ReactDOM from "react-dom";
+import { useState } from "react";
+import { render } from "react-dom";
 
 interface AppProps {
-  greet: string
+  greet: string;
 }
 
-class App extends Component<AppProps> {
-  render(): ReactNode {
-    return <div>{this.props.greet}</div>
-  }
-}
+const App: React.FC<AppProps> = ({ greet }) => {
+  const [counter, setCounter] = useState(0);
 
-ReactDOM.render(<App greet="Hi there!" />, document.querySelector("#root"))
+  const onIncrement = (): void => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  const onDecrement = (): void => {
+    setCounter((prevCounter) => prevCounter - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={onIncrement}>Increment</button>
+      <button onClick={onDecrement}>Decrement</button>
+      {counter}
+
+      <div>{greet}</div>
+    </div>
+  );
+};
+
+render(<App greet="Hi there!" />, document.querySelector("#root"));
